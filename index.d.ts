83c0137caeb54689a024f39425b41185461156a7
claytonsilva <clayton@xdevel.com.br>
2017-01-24 18:14:41 -0200

    feito ajuste do indice das referencias

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,9 @@
-import * as config from './src/config';
-export {config};
-// declare namespace config {
-    
-// }
+import * as Config from './src/config'
+import * as Controllers from './src/controllers'
+import * as Interfaces from './src/interfaces'
+import * as Models from './src/models'
+import * as Routes from './src/routes'
+import * as Auth from './src/auth'
 
+export {Config, Controllers, Interfaces, Models, Routes, Auth};
 
-// export {BasePersistController,ForgotController,SignupController} from './src/controllers'
-// export {IBaseModel,
-//         IBaseUser,
-//         IDAO,
-//         IForgot,
-//         IPersistController,
-//         IResultSearch} from './src/interfaces'
-// export {BaseModel,DAO,ForgotDAO,SignUpDAO} from './src/models'
-// export {BaseRouter,ForgotRouter,LoginRouter,PersistRouter,SignupRouter} from './src/routes'
-// export {authenticate,jwtGenerator,passportJwt} from './src/auth'
