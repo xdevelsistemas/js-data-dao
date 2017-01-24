ab7cb11692e8572d48207ca2d04458c592ec9308
claytonsilva <clayton@xdevel.com.br>
2017-01-24 18:41:33 -0200

    nova versao

--- index.d.ts
+++ index.d.ts
@@ -4,6 +4,8 @@ import * as Interfaces from './src/interfaces'
 import * as Models from './src/models'
 import * as Routes from './src/routes'
 import * as Auth from './src/auth'
+import * as Services from './src/services'
 
-export {Config, Controllers, Interfaces, Models, Routes, Auth};
+export {Config, Controllers, Interfaces, Models, Routes, Auth, Services}
+
 
