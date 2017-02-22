15154b52f0310917d14f546310d3ac85bb2fb1ab
claytonsilva <clayton@xdevel.com.br>
2017-02-22 18:02:39 -0300

    fix deps

--- lib/routes/base-router.ts
+++ lib/routes/base-router.ts
@@ -5,12 +5,12 @@ import { IPersistController } from '../interfaces'
 import * as JSData from 'js-data'
 
 export class BaseRouter {
-  respond(t: Promise<any>, res: Response): Promise<Response> {
+  respond( t: Promise<any>, res: Response ): Promise<Response> {
     return t
-      .then((u) => res.json(u))
-      .catch((err: APIError) => res.status(err.statusCode).json({ error: err.error, objectResponse: err.objectResponse}))
-      .catch((err: Error) => res.status(500).json(err.message))
-      .catch((err) => res.status(500).json(err))
+      .then(( u ) => res.json( u ) )
+      .catch(( err: APIError ) => res.status( err.statusCode ).json( { error: err.error, objectResponse: err.objectResponse }) )
+      .catch(( err: Error ) => res.status( 500 ).json( err.message ) )
+      .catch(( err ) => res.status( 500 ).json( err ) )
   }
 }
 
@@ -18,7 +18,7 @@ export class PersistRouter<M extends IBaseModel, C extends IPersistController<M>
   controller: C
   router: Router
 
-  constructor(store: JSData.DataStore, controller: C) {
+  constructor( store: JSData.DataStore, controller: C ) {
     super()
     this.controller = controller
     this.router = Router()
@@ -28,22 +28,22 @@ export class PersistRouter<M extends IBaseModel, C extends IPersistController<M>
   public routers() {
     let ctrl = this.controller
     /* GET lista todos os registros da classe corrente em controller. */
-    this.router.get('/', (req, res, next) => this.respond(ctrl.findAll(req, res, next), res))
+    this.router.get( '/', ( req, res, next ) => this.respond( ctrl.findAll( req, res, next ), res ) )
 
     /* GET busca o registro com o id. */
-    this.router.get('/:id', (req, res, next) => this.respond(ctrl.find(req, res, next), res))
+    this.router.get( '/:id', ( req, res, next ) => this.respond( ctrl.find( req, res, next ), res ) )
 
     /* POST cria um novo registro da classe corrente em controller. */
-    this.router.post('/', (req, res, next) => this.respond(ctrl.create(req, res, next), res))
+    this.router.post( '/', ( req, res, next ) => this.respond( ctrl.create( req, res, next ), res ) )
 
     /* PUT atualiza o registro. */
-    this.router.put('/:id', (req, res, next) => this.respond(ctrl.update(req, res, next), res))
+    this.router.put( '/:id', ( req, res, next ) => this.respond( ctrl.update( req, res, next ), res ) )
 
     /* DELETE deleta o registro com o id. */
-    this.router.delete('/:id', (req, res, next) => this.respond(ctrl.delete(req, res, next), res))
+    this.router.delete( '/:id', ( req, res, next ) => this.respond( ctrl.delete( req, res, next ), res ) )
 
     /* POST lista paginada com os registros da classe corrente em controller. */
-    this.router.post('/query', (req, res, next) => this.respond(ctrl.query(req, res, next), res))
+    this.router.post( '/query', ( req, res, next ) => this.respond( ctrl.query( req, res, next ), res ) )
   }
 
   public getRouter(): Router {
